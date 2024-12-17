a0960b3e45461b8edcc6907a003133f6ca3e1877
nhatda <nhatda@frontiersoftwareado.com>
2024-12-17 22:14:23 +0700

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './auth/jwt.strategy';
 import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    FoodModule,
-    PassportModule,
-    JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
+  imports: [AuthModule, FoodModule, PassportModule],
   providers: [JwtStrategy],
 })
 export class AppModule {}
